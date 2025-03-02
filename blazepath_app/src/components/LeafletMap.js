2fa75b90dead4bcb758059da849766485a45ebed
michaelkong0 <the.evolutions65@gmail.com>
2025-03-02 17:18:33 -0500

    Add fire perimeters and API scraping

--- blazepath_app/src/components/LeafletMap.js
+++ blazepath_app/src/components/LeafletMap.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "../styles/LeafletMap.css";
+import geoJsonData from '../data/allFirePerims.json';
+
+const setColor = ({ properties }) => {
+  return { weight: 1, fillColor:"FF6912" };
+};
 
 const LeafletMap = ({ selectedFactor }) => {
   return (
     <div className="map-container">
-      <MapContainer center={[37.7749, -122.4194]} zoom={13} className="map">
+      <MapContainer center={[34.0365485,-118.2507257]} zoom={10} className="map">
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
+        <GeoJSON data={geoJsonData} style={setColor}/>
       </MapContainer>
       <div className="map-controls">
         <button className="control-button">📍</button>
