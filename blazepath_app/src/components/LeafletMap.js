d9cbbd152051cd79b8df6456ac12797fc13c1b1d
michaelkong0 <the.evolutions65@gmail.com>
2025-03-02 21:57:20 -0500

    Add scraping

--- blazepath_app/src/components/LeafletMap.js
+++ blazepath_app/src/components/LeafletMap.js
@@ -8,7 +8,22 @@ const setColor = ({ properties }) => {
   return { weight: 1, fillColor:"FF6912" };
 };
 
+
+
+async function getFireData() {
+  try {
+    const response = await fetch('https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/WFIGS_Interagency_Perimeters_Current/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson');
+    const responseJson = await response.json();
+    console.log(responseJson)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
 const LeafletMap = ({ selectedFactor }) => {
+
+
   return (
     <div className="map-container">
       <MapContainer center={[34.0365485,-118.2507257]} zoom={10} className="map">
